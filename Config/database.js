e6d8078c5ac4bc77938c2d747189cdb80006e762
Harshitha1r <harshithar2009@gmail.com>
2025-05-24 20:39:45 +0530

    Created Login,signup,Profile api

---
+++ Config/database.js
@@ -0,0 +1,11 @@
+const mongodb=require('mongoose')
+
+const mongodb_connection_url="mongodb+srv://harshitha:harshitha@dbcluster.eghu41z.mongodb.net/devTinder"
+
+const ConnectDB=async()=>{
+    await mongodb.connect(mongodb_connection_url);
+}
+
+
+module.exports=ConnectDB
+
